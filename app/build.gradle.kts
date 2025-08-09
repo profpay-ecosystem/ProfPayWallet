1bbe5d743df46db541bfc9f1584a215788f3bd7e
ProfPay Ecosystem <profpayecosystem@gmail.com>
2025-08-10 01:02:48 +0300

    Updated app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 //        MAJOR: Внесение изменений, ломающих обратную совместимость.
 //        MINOR: Добавление новых функций без нарушения совместимости.
 //        PATCH: Исправление ошибок и незначительные улучшения без изменения функциональности.
-        versionName = "3.3.11" // MAJOR.MINOR.PATCH
+        versionName = "3.3.12" // MAJOR.MINOR.PATCH
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
